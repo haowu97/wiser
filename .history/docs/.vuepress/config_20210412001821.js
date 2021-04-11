b93e40f06e5210e18aba08d4ffe013868c75a8cc
henrywu97 <wuhaozuel@gmail.com>
2021-04-12 00:23:44 +0800

    8

---
+++ .history/docs/.vuepress/config_20210412001821.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: '量化投资策略与技术',
+    description: 'Just playing around',
+    // extendMarkdown: md => {
+    //   md.set({
+    //       html: true
+    //   })
+    //   md.use(require('markdown-it-katex'))
+    //   }
+}
