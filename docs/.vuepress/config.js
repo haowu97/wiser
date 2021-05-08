524a3814259bf55806c19272f3e39cc995bb7a60
imhyacinth <467303976@qq.com>
2021-05-08 21:42:18 +0800

    '20210502'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,10 +53,13 @@ module.exports = {
           sidebarDepth: 0,    // 可选的, 默认值是 1
           children: [
             { title: '本章介绍', path:'/QuantStrategy&Technology/Chapter3/'},
+            { title: '3.0 量化择时方法梳理', path:'/QuantStrategy&Technology/Chapter3/3.0量化择时方法梳理'},
             { title: '3.1 趋势追踪', path:'/QuantStrategy&Technology/Chapter3/3.1趋势追踪'},
             { title: '3.2 市场情绪', path:'/QuantStrategy&Technology/Chapter3/3.2市场情绪'},
+            { title: '3.3 时变夏普率', path:'/QuantStrategy&Technology/Chapter3/3.3时变夏普率'},
             { title: '3.4 牛熊线', path:'/QuantStrategy&Technology/Chapter3/3.4牛熊线'},
             { title: '3.5 Hurst指数', path:'/QuantStrategy&Technology/Chapter3/3.5Hurst指数'},
+            { title: '3.6 支持向量机', path:'/QuantStrategy&Technology/Chapter3/3.6支持向量机'},
           ]
         },
         {
